d1b891e8c74e46aaa6878e40ca22ac8d10cbeba1
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-01-23 13:43:37 +0100

    Added image loading library

---
+++ include/image.h
@@ -0,0 +1,34 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+#include <stdio.h>
+
+#define TWOPI 6.2831853
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// DO NOT CHANGE THIS FILE
+
+typedef struct{
+    int c,h,w;
+    float *data;
+} image;
+
+
+// Loading and saving
+image make_image(int c, int h, int w);
+image load_image(char *filename);
+void save_image(image im, const char *name);
+void save_png(image im, const char *name);
+void save_image_binary(image im, const char *fname);
+image load_image_binary(const char *fname);
+void save_png(image im, const char *name);
+void free_image(image im);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ src/image-grayscale/imageGray.cu
@@ -0,0 +1,9 @@
+#include "image.h"
+#include <iostream>
+
+int main() {
+
+    image im = load_image("../../data/dog.jpg");
+
+    return 0;
+}
