825dbc44aa1b28da3622731c245dd76931a17c60
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-01-29 16:30:26 +0100

    Added matrix multiplication file naive

---
+++ src/mmul/matrixMul.cu
@@ -0,0 +1,79 @@
+#include "matrix.h"
+#include "image.h"
+#include <iostream>
+#include "cuda_runtime.h"
+#include "helper_cuda.h"
+
+
+int main() {
+
+    image H = make_image(1,12, 3);
+    //cout << m.shallow << endl;
+    matrix g = make_matrix(12, 3);
+    
+
+
+
+    // size_t inputImgBytes = im.c*im.w*im.h*sizeof(decltype(*im.data));
+    // size_t outputImgBytes = im.w*im.h*sizeof(decltype(*im.data));
+
+    // // Error code to check return values for CUDA calls
+    // cudaError_t err = cudaSuccess;
+
+    // // Allocate the device input vector A
+    // float *d_A = NULL;
+    // err = cudaMalloc((void **)&d_A, inputImgBytes);
+
+    // if (err != cudaSuccess) {
+    // fprintf(stderr, "Failed to allocate device vector A (error code %s)!\n",
+    //         cudaGetErrorString(err));
+    // exit(EXIT_FAILURE);
+    // }
+
+    //   // Allocate the device input vector B
+    // float *d_B = NULL;
+    // err = cudaMalloc((void **)&d_B, outputImgBytes);
+
+    // if (err != cudaSuccess) {
+    //     fprintf(stderr, "Failed to allocate device vector B (error code %s)!\n",
+    //             cudaGetErrorString(err));
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // err = cudaMemcpy(d_A, im.data, inputImgBytes, cudaMemcpyHostToDevice);
+
+    // if (err != cudaSuccess) {
+    //     fprintf(stderr,
+    //             "Failed to copy vector A from host to device (error code %s)!\n",
+    //             cudaGetErrorString(err));
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // const dim3 dimGrid((int)ceil((im.w)/16.0), (int)ceil((im.h)/16.0));
+	// const dim3 dimBlock(16, 16, 1);
+
+    // rgbtoGray <<< dimGrid, dimBlock  >>> (d_A, d_B, im.w, im.h);
+
+    // err = cudaGetLastError();
+
+    // if (err != cudaSuccess) {
+    //     fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n",
+    //             cudaGetErrorString(err));
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // cout << "Copy output data from the CUDA device to the host memory" << endl;
+
+    // err = cudaMemcpy(gray.data, d_B, outputImgBytes, cudaMemcpyDeviceToHost);
+
+    // if (err != cudaSuccess) {
+    //     fprintf(stderr,
+    //             "Failed to copy vector C from device to host (error code %s)!\n",
+    //             cudaGetErrorString(err));
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // save_image(gray, (char *)"gray");
+
+    return 0;
+}
